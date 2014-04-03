34f79ac9510a4075c466f929ae277734a4c7e89a
Adrian Westlund <adrianwestlund@hotmail.com>
2014-04-03 13:12:39 +0200

    Ändrat lite mer på resept klassen

--- Kode/Resept.java
+++ Kode/Resept.java
@@ -1,8 +1,8 @@
 /*	Denne klassen er en Reseptpost.
-	Den har en Medisinpeker som peker på den medisin som reseptet er av
-	och en Legepeker på den lege som har skrivit respetet.
+	Klassen har en Medisinpeker som peker på medisinen for reseptet.
+	Legepeker som peker på den lege som har skrivit reseptet.
 	Laget av Adrian Westlund
-	Siste versjon 02-04-2014*/
+	Siste versjon 03-04-2014*/
 
 import java.io.*;
 import java.util.Calendar;
@@ -15,16 +15,17 @@ public class Resept implements Serializable
 	Calendar dato;
 	Lege lege;
 	int reit;
-	//public static final int RESEPTNUMMER;//Kontrollera och behøvs den?
+	private int reseptNr;
 	String beskrivelse;
 	boolean utlevert;
 
-	public Resept(Medisin m, Calendar d, Lege l, int r, String b)
+	public Resept(Medisin m, Calendar d, Lege l, int r, int rNr, String b)
 	{
 		medisin = m;
 		dato = d;
 		lege = l;
 		reit = r;
+		reseptNr = rNr;
 		beskrivelse = b;
 		utlevert = false;
 	}
