dbc033ec85788d2bf25056bbc5dfc53c5d3e6f14
s165519 <s165519@stud.hioa.no>
2014-04-04 13:47:37 +0200

    Lagt til serienummer

--- Kode/PersonComparator.java
+++ Kode/PersonComparator.java
@@ -11,6 +11,8 @@ import java.io.Serializable;
 
 public class PersonComparator implements Comparator<Person>, Serializable
 {
+	private static final long serialVersionUID = 1006L;
+
 	private String rekkefølge = "<\0<0<1<2<3<4<5<6<7<8<9" +
 	                  "<A,a<B,b<C,c<D,d<E,e<F,f<G,g<H,h<I,i<J,j" +
 	                 "<K,k<L,l<M,m<N,n<O,o<P,p<Q,q<R,r<S,s<T,t" +
