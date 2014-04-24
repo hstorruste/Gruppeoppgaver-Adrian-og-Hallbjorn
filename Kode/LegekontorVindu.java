8e188eb0ca85531fa2d6cc8d2064f9aa1649c24b
s165519 <s165519@stud.hioa.no>
2014-04-24 15:02:58 +0200

    Begynte så vidt på legekontorvinduet

---
+++ Kode/LegekontorVindu.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+
+public class LegekontorVindu extends JFrame
+{
+
+	private JTextField fornavnFelt, etternavnFelt, epostFelt epostigjenFelt, gtadrFelt;
+	private JTextField[] tekstFelt;
+	private String[] lableTekst = {"Fornavn", "Etternavn", "Epost"};
+	private JPasswordField passordfelt;
+
+
+	public LegekontorVindu
+	{
+		super("Legekontor");
+
+		for(JTextField temp:
+
+	}
+}
