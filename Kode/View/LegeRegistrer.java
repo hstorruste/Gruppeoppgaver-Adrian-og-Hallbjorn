4f5e3c2f59774d78b99a333904e714fc2ca413ac
s165519 <s165519@stud.hioa.no>
2014-04-29 09:06:23 +0200

    Tok bort en popup

--- Kode/View/LegeRegistrer.java
+++ Kode/View/LegeRegistrer.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -144,8 +144,6 @@ public class LegeRegistrer extends JPanel {
                         } else {
 
                             vindu.tegnFinnPasientGUI(innlogget);
-                            melding = "Det er riktig!";
-                            Komponent.popup(parentFrame, melding);
                         }
                     }
                 } else {
